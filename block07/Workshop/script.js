28e7da9020e1c08df925300f1f17de3d5af1ebef
vtorres789 <victortorres180@gmail.com>
2023-03-23 08:32:40 -0400

    block7 work

---
+++ block07/Workshop/script.js
@@ -0,0 +1,15 @@
+// Print Initial Message
+alert("You have received this message because you have been chosen to open an important vault. Here is the secret combination:");
+
+// Assignment of Variables
+
+const var1 = 100 / 10;
+const var2 = 20 * 2;
+const var3 = 50 - 11;
+
+let text1 = var1.toString();
+let text2 = var2.toString();
+let text3 = var3.toString();
+
+// Output: All together
+alert(text1 + " " + text2 + " " + text3);
---
+++ block07/guided _practice/script.js
@@ -0,0 +1,12 @@
+let numCows = 50;
+const x = ("Hello World");
+console.log("Hello World");
+alert("Hello WOrld");
+const numUmbrellas = 10 * 3;
+const num = 5 + 1;
+alert(num);
+const str = 'Hello' + ' ' + 'World';
+let str1 = 'Hello';
+let str2 = 'I am';
+let nombre = 'Josh';
+alert(str1 + " " + str2 + " " + nombre);
